d6ff11e768bf4cf7c217b9f4244b9434e93a44cb
igargaditya <igargaditya@gmail.com>
2025-07-31 13:36:36 +0530

    Exceptions Added

--- src/main/java/com/aditya/taskManager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aditya/taskManager/exception/GlobalExceptionHandler.java
@@ -1,4 +1,63 @@
 package com.aditya.taskManager.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    // This handles if not present
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleResourceNotFound(ResourceNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return error;
+    }
+
+    // This handles Foreign key constraints
+    @ExceptionHandler(InvalidTaskException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleInvalidTaskException(InvalidTaskException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return error;
+    }
+
+    // This checks for validations of users and tasks
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fe : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fe.getField(), fe.getDefaultMessage());
+        }
+        return errors;
+    }
+
+    // This handles Enums and malformed User inputs
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleInvalidEnum(HttpMessageNotReadableException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Malformed request or invalid enum value");
+        return error;
+    }
+
+    // Any other error - Generic error
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleAll(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Internal Server Error");
+        return error;
+    }
 }
--- src/main/java/com/aditya/taskManager/exception/InvalidTaskException.java
+++ src/main/java/com/aditya/taskManager/exception/InvalidTaskException.java
@@ -1,4 +1,7 @@
 package com.aditya.taskManager.exception;
 
-public class InvalidTaskException {
+public class InvalidTaskException extends RuntimeException {
+    public InvalidTaskException(String message) {
+        super(message);
+    }
 }
